a3e4addd9ef85670323b8d13e10d44d6a87f365b
lqtrung95 <44612846+lequocgiom@users.noreply.github.com>
2020-04-10 12:40:06 +0700

    recreate persistence user.

--- src/App.js
+++ src/App.js
@@ -9,27 +9,14 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { selectCurrentUser } from "./redux/user/user.selector";
+import { checkUserSession } from "./redux/user/user.actions";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    // const { setCurrentUser } = this.props;
-    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-    //   if (userAuth) {
-    //     const userRef = await createUserProfileDocument(userAuth);
-    //     userRef.onSnapshot(snapShot => {
-    //       setCurrentUser({
-    //         currentUser: {
-    //           id: snapShot.id,
-    //           ...snapShot.data()
-    //         }
-    //       });
-    //     });
-    //   } else {
-    //     setCurrentUser(userAuth);
-    //   }
-    // });
+    const { checkUserSession } = this.props;
+    checkUserSession();
   }
 
   componentWillUnmount() {
@@ -52,7 +39,14 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
+  currentUser: selectCurrentUser
 });
 
-export default connect(mapStateToProps, null)(App);
+const mapDispatchToProps = dispatch => ({
+  checkUserSession: () => dispatch(checkUserSession())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,20 +1,24 @@
 import UserActionTypes from "./user.types";
 
 export const googleSignInStart = () => ({
-  type: UserActionTypes.GOOGLE_SIGN_IN_START,
+  type: UserActionTypes.GOOGLE_SIGN_IN_START
 });
 
-export const emailSignInStart = (emailAndPassword) => ({
+export const emailSignInStart = emailAndPassword => ({
   type: UserActionTypes.EMAIL_SIGN_IN_START,
-  payload: emailAndPassword,
+  payload: emailAndPassword
 });
 
-export const signInSuccess = (user) => ({
+export const signInSuccess = user => ({
   type: UserActionTypes.SIGN_IN_SUCCESS,
-  payload: user,
+  payload: user
 });
 
-export const signInFailure = (error) => ({
+export const signInFailure = error => ({
   type: UserActionTypes.SIGN_IN_FAILURE,
-  payload: error,
+  payload: error
+});
+
+export const checkUserSession = () => ({
+  type: UserActionTypes.CHECK_USER_SESSION
 });
